e6c09fa172da7730cad476fec065a3cb3377f445
j1sher <jfishervdv@gmail.com>
2022-09-30 11:58:58 +1300

    dev

--- AT-tracker/backend.py
+++ AT-tracker/backend.py
@@ -146,8 +146,6 @@ def get_live_updates(trip_id):
         data = response.read()
         data_dict = parse_json(data)
         conn.close()
-        if data_dict["response"]["entity"] == []:
-            return "no data"
         return data_dict
 
     except Exception as e:
@@ -157,7 +155,6 @@ def get_live_updates(trip_id):
 def get_next_bus(route_name, stop_name, minutes_to_stop):
     #print("Getting Stop ID")
     stop_id = get_stop_id(stop_name)
-    print("Stop ID: " + stop_id)
     #print("Getting Route ID")
     route_id = get_route_id(stop_id, route_name)
 
@@ -172,7 +169,6 @@ def get_next_bus(route_name, stop_name, minutes_to_stop):
         if stop_time["trip_id"] in trip_ids:
             stop_times.append(stop_time["arrival_time"])
     data = dict(zip(trip_ids, stop_times))
-
     #print("Getting Next Bus Time")
     now = datetime.datetime.now()
     now = now.strftime("%H:%M:%S")
@@ -188,8 +184,6 @@ def get_next_bus(route_name, stop_name, minutes_to_stop):
             if value == min(next_bus):
                 current_trip = key
                 max_time = datetime.datetime.strptime(value, "%H:%M:%S")
-    if next_bus == []:
-        return "no more buses today", "no more buses today"
     next_bus = min(next_bus)
     next_bus = datetime.datetime.strptime(next_bus, "%H:%M:%S")
     next_bus = next_bus.strftime("%I:%M %p")
@@ -197,42 +191,36 @@ def get_next_bus(route_name, stop_name, minutes_to_stop):
 
 #    print("Getting Live Updates")
     live_data = get_live_updates(current_trip)
+    if live_data["response"]["entity"] != []:
+        try:
+            for item in live_data["response"]["entity"]:
+                if item["trip_update"]["trip"]["trip_id"] == current_trip:
+                    bus_stop_id = item["trip_update"]["stop_time_update"]["stop_id"]
+                    bus_stop_name = get_stop_word_name_long(bus_stop_id)
+                    while True:
+                        try:
+                            bus_delay = item["trip_update"]["stop_time_update"]["departure"]["delay"]
+                            break
+                        except:
+                            pass
+                        try:
+                            bus_delay = item["trip_update"]["stop_time_update"]["arrival"]["delay"]
+                            break
+                        except:
+                            print("An error occured when getting bus delay")
+                    bus_delay = bus_delay/60
+                    #Calculate bus time with delay
+                    actual_bus_time = datetime.datetime.strptime(next_bus, "%I:%M %p")
+                    actual_bus_time = actual_bus_time + datetime.timedelta(minutes=bus_delay)
+                    actual_bus_time = actual_bus_time.strftime("%I:%M %p")
+        except:
+            next_bus = []
+    print(next_bus, actual_bus_time)
     if live_data == "no data":
-        print("No data/Bus not on route")
-    else:
-        for item in live_data["response"]["entity"]:
-            if item["trip_update"]["trip"]["trip_id"] == current_trip:
-                bus_stop_id = item["trip_update"]["stop_time_update"]["stop_id"]
-                bus_stop_name = get_stop_word_name_long(stop_name)
-                print("Bus is at", bus_stop_name)
-                while True:
-                    try:
-                        bus_delay = item["trip_update"]["stop_time_update"]["departure"]["delay"]
-                        break
-                    except:
-                        pass
-                    try:
-                        bus_delay = item["trip_update"]["stop_time_update"]["arrival"]["delay"]
-                        break
-                    except:
-                        print("An error occured when getting bus delay")
-                bus_delay = bus_delay/60
-                print("Bus is", round(bus_delay,2), "minutes late")
-                #Calculate bus time with delay
-                actual_bus_time = datetime.datetime.strptime(next_bus, "%I:%M %p")
-                actual_bus_time = actual_bus_time + datetime.timedelta(minutes=bus_delay)
-                actual_bus_time = actual_bus_time.strftime("%I:%M %p")
-                print("Bus is due at", actual_bus_time)
-        if actual_bus_time == "":
-            actual_bus_time = next_bus
-    if next_bus == [] or actual_bus_time == "":
-        return "", ""
-    else:
-        given_time = datetime.datetime.strptime(actual_bus_time, "%I:%M %p")
-        final_time = given_time - timedelta(minutes=minutes_to_stop)
-        final_time = final_time.strftime("%I:%M %p")
-        
-        return next_bus, actual_bus_time, final_time
+        print("No live data")
+    if next_bus == []:
+        print("No active bus found")
+    return str(next_bus), str(actual_bus_time)
 
 if __name__ == "__main__":
     start_time = time.time()
